e1bfb69120c7fcaaea38bf9ae18192d43e077786
vvladikk <73952693+vvladikk@users.noreply.github.com>
2023-03-24 18:31:05 +0200

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,25 @@
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+    add(itemName) {
+      console.log(`Adding ${itemName} to inventory`);
+      this.items.push(itemName);
+    },
+    remove(itemName) {
+      console.log(`Removing ${itemName} from inventory`);
+      this.items = this.items.filter(item => item !== itemName);
+    },
+  };
+  
+  const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action.bind(inventory)(itemName);
+  };
+  
+  invokeInventoryAction('Medkit', inventory.add);
+  
+  console.log(inventory.items); 
+  
+  invokeInventoryAction('Gas mask', inventory.remove);
+ 
+  console.log(inventory.items); 
+  
